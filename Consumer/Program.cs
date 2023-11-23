52a3f970e39ed34f54259c2288a9ee6db38fb695
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-11-23 17:08:26 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,77 @@
+﻿// CONSUMER
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://..");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P2P (Point-to-Point) Message Desing
+
+//string queueName = "example-p2p-queue";
+
+//channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false);
+
+//EventingBasicConsumer consumer = new(channel);
+
+//channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
+#endregion
+
+#region Publish/Subscribe (Pub/Sub) Message Design
+
+//string exchangeName = "example-pub-sub-exchange";
+
+//channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+//string queueName = channel.QueueDeclare().QueueName;
+
+//channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: string.Empty);
+
+//EventingBasicConsumer consumer = new(channel);
+
+//channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+//byte[] message = Encoding.UTF8.GetBytes("I am pub-sub message design");
+
+//channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty, body: message);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
+#endregion
+
+#region Work Queue Message Design
+
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false);
+
+//EventingBasicConsumer consumer = new(channel);
+
+//channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+//channel.BasicQos(prefetchCount: 1, prefetchSize: 0, global: false);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
+
+#endregion
+
+
+
+Console.Read();
---
+++ Publisher/Program.cs
@@ -0,0 +1,65 @@
+﻿// PUBLISHER
+
+using RabbitMQ.Client;
+using System.Globalization;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://..");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+#region P2P (Point-to-Point) Message Desing
+
+//string queueName = "example-p2p-queue";
+
+//channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false);
+
+//byte[] message = Encoding.UTF8.GetBytes("I am p2p queue design");
+
+//channel.BasicPublish(exchange: string.Empty, routingKey: queueName, body: message);
+
+#endregion
+
+#region Publish/Subscribe (Pub/Sub) Message Design
+
+//string exchangeName = "example-pub-sub-exchange";
+
+//channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(200);
+
+//    byte[] message = Encoding.UTF8.GetBytes(i + " I am pub-sub message design ");
+
+//    channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty, body: message);
+//}
+
+#endregion
+
+#region Work Queue Message Design
+
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false);
+
+//IBasicProperties properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
+
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(200);
+
+//    byte[] message = Encoding.UTF8.GetBytes(i + " I am Work Queue Message Design");
+
+//    channel.BasicPublish(exchange: string.Empty, routingKey: queueName, body: message, basicProperties: properties);
+//}
+
+#endregion
+
+
+
+Console.Read();
